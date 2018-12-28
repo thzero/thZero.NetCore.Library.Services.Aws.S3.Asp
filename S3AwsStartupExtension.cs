f111aa7a2771ae5f74545f5c5df6c9f229f6e904
thZero.com <development@thzero.com>
2018-12-28 10:41:19 -0600

    Initial

---
+++ S3AwsStartupExtension.cs
@@ -0,0 +1,40 @@
+﻿/* ------------------------------------------------------------------------- *
+thZero.NetCore.Library.Factories.DryIoc
+Copyright (C) 2016-2018 thZero.com
+
+<development [at] thzero [dot] com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ * ------------------------------------------------------------------------- */
+
+using System;
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+using Amazon.S3;
+
+namespace thZero.AspNetCore
+{
+    public class S3AwsStartupExtension : AwsStartupExtension
+    {
+        #region Protected Methods
+        public override void ConfigureServicesInitializeMvcPost(IServiceCollection services, IConfiguration configuration)
+        {
+            base.ConfigureServicesInitializeMvcPost(services, configuration);
+
+            services.AddAWSService<IAmazonS3>();
+        }
+        #endregion
+    }
+}
